4fec54347ade66382b04ab86e47da98695f30fe3
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2025-03-08 23:25:23 -0800

    created Sends To World Site

--- assets/js/App.js
+++ assets/js/App.js
@@ -4,3 +4,7 @@ toggle.addEventListener('click' , ()=>{
     menu.classList.add('active')
 })
 
+const btnclose=document.getElementById('close')
+btnclose.addEventListener('click' , ()=>{
+    menu.classList.remove('active')
+})
